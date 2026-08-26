13252e33aa78ae72def62a1309cd843afeec2079
Amateur Programmer <solo2244rt@gmail.com>
2026-08-26 16:55:07 +0700

    Add ring.h header file with ring segment offsets

---
+++ bootloader/ring.h
@@ -0,0 +1,14 @@
+#ifndef RING_H
+#define RING_H
+
+#define RING_SEGMENTS 16
+
+static const int RingOffsetX[RING_SEGMENTS] = {
+0,54,99,129,140,129,99,54,0,-54,-99,-129,-140,-129,-99,-54
+};
+
+static const int RingOffsetY[RING_SEGMENTS] = {
+-140,-129,-99,-54,0,54,99,129,140,129,99,54,0,-54,-99,-129
+};
+
+#endif
