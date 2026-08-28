71345474cbce99980b3e995fae6a2ebdacf66d40
Amateur Programmer <solo2244rt@gmail.com>
2026-08-28 18:36:41 +0700

    Modify kernel entry point and implement multiplication
    Updated entry point and added multiplication logic.

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,8 +1,18 @@
 BITS 64
-ORG 0x0
+ORG 0x100000
 
-start:
-    cli
+entry:
+    mov rbx, [rdi+32]
+    mov rcx, [rdi+24]
+    mov r8, rbx
+    imul r8, rcx
+    mov r9, [rdi]
+    mov rdi, r9
+    mov ecx, r8d
+    mov eax, 0x0000FF00
+    rep stosd
+
 .hang:
+    cli
     hlt
     jmp .hang
