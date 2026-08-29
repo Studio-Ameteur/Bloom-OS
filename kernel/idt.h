8e2bdab18c374ad690cdd9fc38a625c3fd3d141f
Amateur Programmer <solo2244rt@gmail.com>
2026-08-29 08:49:19 +0700

    Update idt.h

--- kernel/idt.h
+++ kernel/idt.h
@@ -2,8 +2,12 @@
 
 #include <stdint.h>
 
+typedef void (*IRQ_HANDLER)(void);
+
 extern volatile uint64_t TickCount;
 
 void InitIdt(void);
 void EnableInterrupts(void);
 void Sleep(uint64_t Milliseconds);
+void RegisterIrqHandler(int Irq, IRQ_HANDLER Handler);
+void UnmaskIrq(int Irq);
