1ddc45bc63cf44b5cc7c0f53de69b6d2e5158a10
Amateur Programmer <solo2244rt@gmail.com>
2026-09-05 10:52:59 +0700

    Add memory map fields to BOOT_INFO structure

--- bootloader/boot_info.h
+++ bootloader/boot_info.h
@@ -7,6 +7,10 @@ typedef struct {
     UINT64 Width;
     UINT64 Height;
     UINT64 PixelsPerScanLine;
+    UINT64 MemoryMapBase;
+    UINT64 MemoryMapSize;
+    UINT64 MemoryMapDescriptorSize;
+    UINT32 MemoryMapDescriptorVersion;
 } BOOT_INFO;
 
 #define KERNEL_LOAD_ADDRESS 0x100000
