c09ac718a3768ad276db8ba7ee41fed74fc2df39
Amateur Programmer <solo2244rt@gmail.com>
2026-09-05 10:53:34 +0700

    Add memory map fields to BOOT_INFO structure

--- kernel/boot_info.h
+++ kernel/boot_info.h
@@ -9,6 +9,10 @@ typedef struct {
     uint64_t Width;
     uint64_t Height;
     uint64_t PixelsPerScanLine;
+    uint64_t MemoryMapBase;
+    uint64_t MemoryMapSize;
+    uint64_t MemoryMapDescriptorSize;
+    uint32_t MemoryMapDescriptorVersion;
 } BOOT_INFO;
 
 #endif
