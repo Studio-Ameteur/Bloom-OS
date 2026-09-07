a6ce3c4975839ae885edbedde44dba860b21b747
Amateur Programmer <solo2244rt@gmail.com>
2026-09-07 18:01:29 +0700

    Add HDA_LOCATION structure and FindHdaController function

--- kernel/pci.h
+++ kernel/pci.h
@@ -13,3 +13,15 @@ typedef struct {
 } AHCI_LOCATION;
 
 AHCI_LOCATION FindAhciController(void);
+
+typedef struct {
+    uint8_t Bus;
+    uint8_t Device;
+    uint8_t Function;
+    uint16_t VendorId;
+    uint16_t DeviceId;
+    uint32_t Bar0;
+    int Found;
+} HDA_LOCATION;
+
+HDA_LOCATION FindHdaController(void);
