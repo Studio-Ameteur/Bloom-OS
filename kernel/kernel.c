48e52beb35f453876733fd9c1f917916b8555117
Amateur Programmer <solo2244rt@gmail.com>
2026-09-07 17:53:57 +0700

    Implement FAT32 file system support
    Added FAT32 support to read KERNEL.BIN and display its size.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,6 +8,7 @@
 #include "pmm.h"
 #include "pci.h"
 #include "ahci.h"
+#include "fat32.h"
 
 static volatile uint32_t *FrameBuffer;
 static uint64_t ScreenWidth;
@@ -295,6 +296,19 @@ kmain(BOOT_INFO *Info)
             if (AhciReadSectors(0, 1, SectorBuffer)) {
                 if (SectorBuffer[510] == 0x55 && SectorBuffer[511] == 0xAA) {
                     DrawString(20, 50, "Disk read OK, boot sig valid", TextColor);
+
+                    if (FatInit()) {
+                        FAT_FILE File = FatFindFile("KERNEL  BIN");
+
+                        if (File.Found) {
+                            DrawString(20, 70, "FAT32: KERNEL.BIN size ", TextColor);
+                            DrawUInt64(20 + 23 * FONT_WIDTH, 70, File.Size, TextColor);
+                        } else {
+                            DrawString(20, 70, "FAT32: KERNEL.BIN not found", TextColor);
+                        }
+                    } else {
+                        DrawString(20, 70, "FAT32 init failed", TextColor);
+                    }
                 } else {
                     DrawString(20, 50, "Disk read OK, bad boot sig", TextColor);
                 }
